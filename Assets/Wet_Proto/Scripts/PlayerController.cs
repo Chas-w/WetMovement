c4303ad99224d8ab1ef158e2d9fb3c1409ede756
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2025-06-04 17:10:41 -0400

    kms

--- Assets/Wet_Proto/Scripts/PlayerController.cs
+++ Assets/Wet_Proto/Scripts/PlayerController.cs
@@ -3,27 +3,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("PlayerProperties")]
-    [SerializeField] float playerMaxSpeed = 10.0f;
-    [SerializeField] float playerAcceleration = 1.0f;
-    [SerializeField] float jumpHeight = 1.0f;
-    [SerializeField] float gravityValue = -9.81f;
-    [SerializeField] float rotationSpeed;
-
-
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private Transform cameraMainTransform;
-    private Rigidbody rigidBody;
     public ControlsInput getInputs;
 
-    Vector3 move;
+    [Header("Player Properties")]
+    [SerializeField] Transform cam;
+    [SerializeField] float maxSpeed = 5;
+    [SerializeField] float moveForce;
+    [SerializeField] float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+    
+    Rigidbody rigidBody;
+    Vector3 direction;
+    Vector3 moveDir;
+
+    bool canMove = false;
 
     private void Awake()
     {
         getInputs = new ControlsInput();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
     }
 
     private void OnEnable()
@@ -35,39 +36,40 @@ public class PlayerController : MonoBehaviour
         getInputs.Disable();
     }
 
+
     private void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>();
-        cameraMainTransform = Camera.main.transform;
-
-        rigidBody = this.GetComponent<Rigidbody>();
-        //rigidBody.maxLinearVelocity = playerMaxSpeed;
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.maxLinearVelocity = maxSpeed;
     }
 
-    void Update()
+    private void Update()
     {
         Vector2 movement = getInputs.Player.Move.ReadValue<Vector2>();
-        move = new Vector3(movement.x, 0, movement.y).normalized;
+        direction = new Vector3(movement.x, 0, movement.y).normalized;
 
-
-        //controller.Move(move * Time.deltaTime * playerMaxSpeed);
-
-
-
-        if (movement.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg;
-            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y ; 
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
 
-            move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
+            moveDir = Quaternion.Euler(0f,targetAngle, 0f) * Vector3.forward;
+            canMove = true;
+        } else
+        {
+            canMove = false;
         }
 
+        //if (rigidBody.linearVelocity.magnitude > maxSpeed) canMove = false;
 
     }
 
     private void FixedUpdate()
     {
-        rigidBody.AddForce(move * playerAcceleration, ForceMode.Force);
+        if (canMove)
+        {
+            rigidBody.AddForce(moveDir.normalized * moveForce);
+        } 
     }
 }
